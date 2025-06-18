e7bf933b633d673c59c54a7f01e40e7c3c2e4f50
Artem S <sartemw@gmail.com>
2025-06-18 17:30:12 +0700

    paw_click animation and Paw.cs done

---
+++ Assets/_Project/CodeBase/Tutorials/Paw.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+namespace _Project.CodeBase.Tutorials
+{
+    public class Paw : MonoBehaviour
+    {
+        public GameObject Ring;
+        public Transform ClickTransform;
+        
+        private const string ClickAnimation = "Click";
+        private Animator _animator;
+        private SpriteSkin _spriteSkin;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _spriteSkin = GetComponent<SpriteSkin>();
+        }
+
+        public void Click() => 
+            _animator.Play(ClickAnimation);
+        
+
+        public void CreateRing()
+        {
+            GameObject ring = Instantiate(Ring, ClickTransform.position, ClickTransform.rotation, gameObject.transform);
+
+            Destroy(ring, 2f);
+        }
+    }
+}
