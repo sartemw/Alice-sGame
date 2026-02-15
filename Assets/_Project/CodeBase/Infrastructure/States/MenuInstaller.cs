40d95348c13cbc8c0f9a450eca21cb438c827ee2
Artem S <sartemw@gmail.com>
2026-02-15 21:25:41 +0700

    audio service modified

---
+++ Assets/_Project/CodeBase/Infrastructure/States/MenuInstaller.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.CodeBase.Infrastructure.States
+{
+    public class MenuInstaller
+    {
+        
+    }
+}
---
+++ Assets/_Project/CodeBase/Services/Audio/SoundPlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Project.CodeBase.Services.Audio
+{
+    public class SoundPlayer : MonoBehaviour
+    {
+        
+    }
+}
